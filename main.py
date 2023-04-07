4e77033d282431e1225f192d3e0a2fb47c83eb64
Haider Ali Khan <haiderk0987@gmail.com>
2023-04-07 14:57:36 +0500

    fix: encode characters in utf-8

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def convert_dict_to_csv(data):
 		end_name = col.split('/')[-1]
 		df.rename(columns = {col:end_name}, inplace = True)
 
-	df.to_csv('result.csv', sep=';')
+	df.to_csv('result.csv', sep=';', encoding='utf-8')
 
 
 if __name__ == "__main__":
